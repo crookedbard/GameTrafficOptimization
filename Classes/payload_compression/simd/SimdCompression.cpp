f77315db6d7bfa83d9865154a7a17dcabc933fe2
Dainius Kreivys <dainiusu@live.com>
2017-01-17 16:35:28 +0200

    Added SIMD library
    still requires configuration. Needs additional libraries.

---
+++ Classes/payload_compression/simd/SimdCompression.cpp
@@ -0,0 +1,26 @@
+//
+//  SimdCompression.cpp
+//  GameTrafficOptimization
+//
+//  Created by Dainius Kreivys on 2017-01-15.
+//
+//
+
+#include "SimdCompression.hpp"
+//#if (CC_TARGET_PLATFORM != CC_PLATFORM_IOS)
+//#include "lz4_stream.h"
+//#endif
+//#include "codecfactory.h"
+//#include "intersection.h"
+
+#include <sstream>
+
+std::string SimdCompression::encode(std::string buffer)
+{
+	return buffer;
+}
+
+std::string SimdCompression::decode(std::string buffer)
+{
+	return buffer;
+}
---
+++ Classes/payload_compression/simd/SimdCompression.hpp
@@ -0,0 +1,22 @@
+//
+//  SimdCompression.hpp
+//  GameTrafficOptimization
+//
+//  Created by Dainius Kreivys on 2017-01-15.
+//
+//
+#ifndef SimdCompression_hpp
+#define SimdCompression_hpp
+
+#include <stdio.h>
+#include <iostream>
+#include <string>
+
+class SimdCompression
+{
+public:
+    static std::string encode(std::string buffer);
+    static std::string decode(std::string buffer);
+};
+
+#endif /* SimdCompression_hpp */
