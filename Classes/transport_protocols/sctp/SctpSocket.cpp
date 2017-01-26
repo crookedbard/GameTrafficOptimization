5246182d8ed8334426d3bc06afb68732f3bf8b5e
Dainius Kreivys <dainiusu@live.com>
2017-01-26 17:35:36 +0200

    starting sctp implementation of basic operations

---
+++ Classes/transport_protocols/sctp/SctpSocket.cpp
@@ -0,0 +1,27 @@
+﻿#include "SctpSocket.h"
+//#if (CC_TARGET_PLATFORM != CC_PLATFORM_IOS)
+#include <cstdlib>
+#include <cstdio>
+#include <corecrt_io.h>
+
+static int receive_cb(struct socket *sock, union sctp_sockstore addr, void *data,
+	size_t datalen, struct sctp_rcvinfo rcv, int flags, void *ulp_info)
+{
+	if (data == nullptr) {
+		done = 1;
+		usrsctp_close(sock);
+	}
+	else {
+#ifdef _WIN32
+		_write(_fileno(stdout), data, (unsigned int)datalen);
+#else
+		if (write(fileno(stdout), data, datalen) < 0) {
+			perror("write");
+		}
+#endif
+		free(data);
+	}
+	return (1);
+}
+
+//#endif
---
+++ Classes/transport_protocols/sctp/SctpSocket.h
@@ -0,0 +1,41 @@
+﻿//
+//  SctpSocket.hpp
+//  GameTrafficOptimization
+//
+//  Created by Dainius Kreivys on 2017-01-26.
+//
+//
+
+#ifndef SctpSocket_hpp
+#define SctpSocket_hpp
+
+//#if (CC_TARGET_PLATFORM != CC_PLATFORM_IOS)
+
+#if (CC_TARGET_PLATFORM == CC_PLATFORM_WIN32)
+#include "transport_protocols/tcp/common.h"
+#include "transport_protocols/tcp/buffer.h"
+#else
+#include "common.h"
+#include "buffer.h"
+#endif
+#include <usrsctp.h>
+
+struct socket *sock;
+struct sockaddr *addr, *addrs;
+struct sockaddr_in addr4;
+struct sockaddr_in6 addr6;
+struct sctp_udpencaps encaps;
+//struct sctpstat stat;
+char buffer[80];
+int i, n;
+int done = 0;
+
+class SctpSocket
+{
+public:
+	
+};
+
+//#endif
+
+#endif
